b2958d0ffe6cbbdc475a1d6f3b36cb8b4e2ab6dc
Ch-Yanghl <hailong.yang@changhong.com>
2017-03-27 14:15:34 +0800

    Initial Commit

---
+++ CaComExpand/CaComExpand.h
@@ -0,0 +1,19 @@
+//
+//  CaComExpand.h
+//  CaComExpand
+//
+//  Created by Ch-Yanghl on 17/3/27.
+//  Copyright © 2017年 HaiLong.Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CaComExpand.
+FOUNDATION_EXPORT double CaComExpandVersionNumber;
+
+//! Project version string for CaComExpand.
+FOUNDATION_EXPORT const unsigned char CaComExpandVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CaComExpand/PublicHeader.h>
+
+
